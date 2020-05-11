bc6c4d455874fe9d411d7db23a5c992a9dc500a5
LancerComet <chw644@hotmail.com>
2020-05-11 20:19:14 +0800

    + Update.

---
+++ 1. JScript/6. network-info/network-info.js
@@ -0,0 +1,26 @@
+// Code from https://www.informit.com/articles/article.aspx?p=26655&seqNum=5
+
+var WshNetwork = WScript.CreateObject('WScript.Network')
+var Drives = WshNetwork.EnumNetworkDrives()
+var Printers = WshNetwork.EnumPrinterConnections()
+
+WScript.Echo('User Domain is: ' + WshNetwork.UserDomain)
+WScript.Echo('Computer Name is: ' + WshNetwork.ComputerName)
+WScript.Echo('User Name is: ' + WshNetwork.UserName)
+
+WScript.Echo('')
+WScript.Echo('Network drive mappings on computer: ')
+
+for (var i = 0; i < Drives.Count(); i += 2) {
+  WScript.Echo('Drive ' + Drives.Item(i) + ' = ' + Drives.Item(i + 1))
+}
+
+WScript.Echo('')
+WScript.Echo('Network printer mappings on computer: ')
+
+for (var i=0; i<Printers.Count(); i += 2) {
+  WScript.Echo('Port ' + Printers.Item(i) + ' = ' + Printers.Item(i + 1))
+}
+
+WScript.Echo('')
+WScript.Echo('Done.')
